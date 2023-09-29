2ee85b80ce4f512e6167d2848d799460b3128128
tuannvt <tuanvancit@gmail.com>
2023-09-29 19:50:27 +0700

    add file

--- src/main/java/mvc/Entity/ProductEntity.java
+++ src/main/java/mvc/Entity/ProductEntity.java
@@ -1,2 +1,73 @@
-package mvc.Entity;public class ProductEntity {
+package mvc.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+  @Column(name = "name")
+  private String name;
+  @Column(name = "description")
+  private String description;
+  @Column(name = "price")
+  private double price;
+  @OneToMany(mappedBy = "products",fetch = FetchType.EAGER)
+  private List<OrderDetailsEntity>orderDetailsEntityList=new ArrayList<>();
+  public ProductEntity(){
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public List<OrderDetailsEntity> getOrderDetailsEntityList() {
+    return orderDetailsEntityList;
+  }
+
+  public void setOrderDetailsEntityList(
+      List<OrderDetailsEntity> orderDetailsEntityList) {
+    this.orderDetailsEntityList = orderDetailsEntityList;
+  }
 }
