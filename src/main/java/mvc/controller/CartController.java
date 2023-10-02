108dfd8707a03991eb16cc312ebc41013b8b8baf
tuannvt <tuanvancit@gmail.com>
2023-10-02 15:50:58 +0700

    update file

--- src/main/java/mvc/controller/CartController.java
+++ src/main/java/mvc/controller/CartController.java
@@ -8,7 +8,7 @@ import mvc.Entity.ProductEntity;
 import mvc.repository.OrderDetailsRepository;
 import mvc.repository.OrderRepository;
 import mvc.repository.ProductRepository;
-import mvc.session.Cart;
+import mvc.session.CartItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 
-public class OrderController {
+public class CartController {
   @Autowired
   OrderRepository orderRepository;
   @Autowired
@@ -30,23 +30,72 @@ public class OrderController {
 
 
   @RequestMapping(value = "/add/{id}",method = RequestMethod.GET)
-  public String addSession(@PathVariable int id, HttpSession session){
+  public String addSession(Model model, CartItem item, @PathVariable int id, HttpSession session,HttpServletRequest request){
     ProductEntity product=productRepository.findById(id).get();
-    Cart cart=new Cart();
-    cart.setProduct(product);
-    session.setAttribute("cart",cart);
-    return "redirect:/";
+    //Cart cart = new Cart();
+    List<CartItem> cartList=(List<CartItem>)request.getSession().getAttribute("cartList");
+    if (cartList==null || cartList.isEmpty()){
+      cartList=new ArrayList<>();
+      item.setProduct(product);
+      item.setQuantity(item.getQuantity());
+      cartList.add(item);
+
+    }
+    else {
+        int total = 0;
+        for ( int i = 0 ; i < cartList.size(); i ++){
+          if (id==cartList.get(i).getProduct().getId()){
+            for ( int k = 0 ; k < cartList.size(); k ++) {
+              if(cartList.get(k).getProduct().getId() == id){
+                total = cartList.get(k).getQuantity();
+                break;
+              }
+            }
+            item.setQuantity(total+1);
+            item.setProduct(product);
+            cartList.add(item);
+            cartList.remove(i);
+            break;
+          }else{
+          int ex = 0;
+            for ( int j = 0 ; j < cartList.size(); j ++) {
+              if(cartList.get(i).getProduct().getId() == cartList.get(j).getProduct().getId() && cartList.size() != 1){
+                ex = 1;
+              }
+            }
+            if(ex == 0){
+              item.setProduct(product);
+              item.setQuantity(item.getQuantity());
+              cartList.add(item);
+              break;
+            }
+          }
+        }
+    }
+    session.setAttribute("cartList",cartList);
+    model.addAttribute("cartList",cartList);
+//    return "redirect:/";
+    return "order/cart";
+  }
+  public void getCart(CartItem cart){
+
   }
   @RequestMapping(value = "/cart",method = RequestMethod.GET)
   public String getCart(Model model, HttpServletRequest request) {
-    Cart cart=(Cart) request.getSession().getAttribute("cart");
-    model.addAttribute("msg","Remove");
-    model.addAttribute("cart",cart);
+    List<CartItem> cartList=(List<CartItem>)request.getSession().getAttribute("cartList");
+    model.addAttribute("cartList",cartList);
     return "order/cart";
   }
-  @RequestMapping(value = "/remove")
-  public String removeCart(Model model,HttpServletRequest request) {
-    request.getSession().removeAttribute("product");
-    return "order/cart";
-  }
+//  @RequestMapping(value = "/add/remove/{id}")
+//  public String removeCart(List<Cart> cartList,HttpServletRequest request,@PathVariable int id) {
+//    Cart cart = new Cart();
+//    for (int i = 0; i <= cartList.size(); i++) {
+//      if (id == cartList.get(i).getProduct().getId()) {
+//        cart = cartList.get(i);
+//        request.getSession().getAttribute("cart");
+//      }
+//    }
+//    request.getSession().removeAttribute("cart");
+//    return "order/cart";
+//  }
 }
