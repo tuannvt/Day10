1e6ac531dc6ce1194c14724f9fd04da66ddecd1f
tuannvt <tuanvancit@gmail.com>
2023-10-05 13:16:08 +0700

    update file

--- src/main/java/mvc/controller/CartController.java
+++ src/main/java/mvc/controller/CartController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import mvc.Entity.OrdersEntity;
 import mvc.Entity.ProductEntity;
 import mvc.repository.OrderDetailsRepository;
 import mvc.repository.OrderRepository;
@@ -12,6 +13,7 @@ import mvc.session.CartItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,90 +31,85 @@ public class CartController {
   public String index(){return "index";}
 
 
-  @RequestMapping(value = "/add/{id}",method = RequestMethod.GET)
-  public String addSession(Model model, CartItem item, @PathVariable int id, HttpSession session,HttpServletRequest request){
-    ProductEntity product=productRepository.findById(id).get();
-    //Cart cart = new Cart();
-    List<CartItem> itemList=(List<CartItem>)request.getSession().getAttribute("itemList");
-    if (itemList==null || itemList.isEmpty()){
-      itemList=new ArrayList<>();
-      item.setProduct(product);
-      item.setQuantity(item.getQuantity());
-      itemList.add(item);
+@RequestMapping(value = "/add/{id}",method = RequestMethod.GET)
+  public String addSession(Model model, @PathVariable int id, HttpSession session,HttpServletRequest request){
+  ProductEntity product=productRepository.findById(id).get();
+  List<CartItem> itemList=(List<CartItem>)request.getSession().getAttribute("itemList");
+  if (itemList==null||itemList.isEmpty()){
+    itemList=new ArrayList<>();
+    CartItem item = new CartItem();
+    item.setProduct(product);
+    item.setQuantity(1);
+    itemList.add(item);
+    }else {
+    CartItem item = new CartItem();
+    item.setProduct(product);
 
+    // count quantity
+    CartItem cartTemp = itemList.stream()
+        .filter(p -> id == (p.getProduct().getId()))
+        .findAny()
+        .orElse(null);
+
+    if(cartTemp!=null){
+      itemList.get(itemList.indexOf(cartTemp)).setQuantity(cartTemp.getQuantity() + 1);
     }
     else {
-        int total = 0;
-        for ( int i = 0 ; i < itemList.size(); i ++){
-          if (id==itemList.get(i).getProduct().getId()){
-            for ( int k = 0 ; k < itemList.size(); k ++) {
-              if(itemList.get(k).getProduct().getId() == id){
-                total = itemList.get(k).getQuantity();
-                break;
-              }
-            }
-            item.setQuantity(total+1);
-            item.setProduct(product);
-            itemList.add(item);
-            itemList.remove(i);
-            break;
-          }else{
-          int ex = 0;
-            for ( int j = 0 ; j < itemList.size(); j ++) {
-              if(itemList.get(i).getProduct().getId() == itemList.get(j).getProduct().getId() && itemList.size() != 1){
-                ex = 1;
-              }
-            }
-            if(ex == 0){
-              item.setProduct(product);
-              item.setQuantity(item.getQuantity());
-              itemList.add(item);
-              break;
-            }
-          }
-        }
+      item.setQuantity(1);
+      itemList.add(item);
     }
-    session.setAttribute("itemList",itemList);
-    model.addAttribute("itemList",itemList);
-//    return "redirect:/";
-    return "order/cart";
   }
-//@RequestMapping(value = "/add/{id}",method = RequestMethod.GET)
-//  public String addSession(Model model,CartItem item, @PathVariable int id, HttpSession session,HttpServletRequest request){
-//  ProductEntity product=productRepository.findById(id).get();
-//  List<CartItem> itemList=(List<CartItem>)request.getSession().getAttribute("itemList");
-//  if (itemList==null||itemList.isEmpty()){
-//    itemList=new ArrayList<>();
-//      for (int i=0;i<=itemList.size();i++){
-//        if (itemList.get(i).getProduct().getId()==id){
-//          item.setQuantity(item.getQuantity()+1);
-//        }else {
-//          item.setProduct(product);
-//          item.setQuantity(item.getQuantity());
-//          itemList.add(item);
-//        }
-//      }
-//    }
-//  session.setAttribute("itemList",itemList);
-//  model.addAttribute("itemList",itemList);
-//  return "order/cart";
-//  }
+
+  session.setAttribute("itemList",itemList);
+  model.addAttribute("msg","Remove");
+  model.addAttribute("itemList",itemList);
+  return "order/cart";
+  }
 
 
   @RequestMapping(value = "/cart",method = RequestMethod.GET)
   public String getCart(Model model, HttpServletRequest request) {
     List<CartItem> itemList=(List<CartItem>)request.getSession().getAttribute("itemList");
+    model.addAttribute("msg","Remove");
     model.addAttribute("itemList",itemList);
     return "order/cart";
   }
-//  @RequestMapping(value = "/add/remove/{id}")
-//  public String removeCart(Model model,List<CartItem> itemList,HttpServletRequest request,@PathVariable int id) {
-//    for (int i=0;i<=itemList.size();i++){
-//      if (itemList.get(i).getProduct().getId()==id){
-//        itemList.remove(i);
-//      }
-//    }
-//    model.addAttribute("itemList",itemList);
-//    return "order/cart";
+  @RequestMapping(value = "remove/{id}",method = RequestMethod.GET)
+  public String removeCart(Model model,HttpServletRequest request,@PathVariable int id) {
+    List<CartItem> itemList=(List<CartItem>)request.getSession().getAttribute("itemList");
+    if (itemList==null){
+      itemList=new ArrayList<>();
+      request.getSession().setAttribute("itemList",itemList);
+    }
+    CartItem item=itemList.stream()
+        .filter(p ->id==(p.getProduct().getId()))
+        .findAny()
+        .orElse(null);
+    itemList.remove(item);
+    request.getSession().setAttribute("itemList",itemList);
+    model.addAttribute("itemList",itemList);
+    return "redirect:/cart";
+  }
+  @RequestMapping(value = "/checkout",method = RequestMethod.GET)
+  public String checkOu1t(Model model){
+    model.addAttribute("OrdersEntity", new OrdersEntity());
+    return "order/checkout";
+  }
+  @RequestMapping(value = "/continue",method = RequestMethod.POST)
+  public String checkOut(Model model, OrdersEntity orders){
+    orders.setCustomerName(orders.getCustomerName());
+    orders.setCustomerAddress(orders.getCustomerAddress());
+    orderRepository.save(orders);
+    List<OrdersEntity>ordersList=(List<OrdersEntity>)orderRepository.findAll();
+    model.addAttribute("ordersList",ordersList);
+    return "order/order";
+  }
+
+//  @RequestMapping(value = "add/delete/{id}",method = RequestMethod.GET)
+//  public String delete(@PathVariable int id){
+//    orderRepository.deleteById(id);
+//    return "redirect:/order";
 //  }
+
+
 }
